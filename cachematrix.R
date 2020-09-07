c0f2ac6d1235fa93711e1c5d27294ba60d8ea5d9
Giovanny Jimenez Gomez <gjimenez@unab.edu.co>
2020-09-06 21:40:45 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## akeCacheMatrix function creates a special “matrix” object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+          set <- function(y) {
+            x <<- y
+            i <<- NULL
+          }
+          get <- function() x
+          setinverse <- function(inverse) i <<- inverse
+          getinverse <- function() i
+          list(set = set,
+               get = get,
+               setinverse = setinverse,
+               getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Cachemean function computes the inverse of the special “matrix”. Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmean()
+          if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+          }
+          data <- x$get()
+          m <- mean(data, ...)
+          x$setmean(m)
+          m
 }
